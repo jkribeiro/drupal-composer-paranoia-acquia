26ca3c3a527fd92a074aaf4eb4bfe92a773e58cc
Suzy Masri <suzymasri@gmail.com>
2020-12-09 13:22:38 +0200

    Composer 2.x support

--- src/Plugin.php
+++ src/Plugin.php
@@ -34,6 +34,18 @@ class Plugin implements PluginInterface, EventSubscriberInterface, Capable {
   /**
    * {@inheritdoc}
    */
+  public function deactivate(Composer $composer, IOInterface $io) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function uninstall(Composer $composer, IOInterface $io) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function getCapabilities() {
     return array(
       'Composer\Plugin\Capability\CommandProvider' => 'Jkribeiro\DrupalComposerParanoiaAcquia\CommandProvider',
